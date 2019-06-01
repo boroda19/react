9c8b484a3f80306f3ad2c3537d87832e9f785611
a-prokopenko <a.boroda19@gmail.com>
2019-06-01 18:46:27 +0300

    lesson 2

---
+++ source/components/StatusBar/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import Styles from './styles.m.css';
+
+export default class Feed extends Component {
+    render() {
+        return (
+            <section className = { Styles.statusBar }></section>
+        );
+    }
+}
